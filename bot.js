a7d9f338cffa6a7aad00ab323b7a5a01f1deeb9e
Subhasish Biswasray <42065821+subhasish9583672357@users.noreply.github.com>
2020-06-29 18:22:53 +0530

    initial

--- bot.js
+++ bot.js
@@ -1,8 +1,8 @@
-var t1=document.getElementsByClassName("Ypffh")[0];
+//var t1=document.getElementsByClassName("Ypffh")[0];
 //var b1=document.getElementsByClassName("sqdOP yWX7d    y3zKF     ")[0];
 //var mes="This is an automatic instagram commenter";
 //t1.textContent=mes;
-alert(t1);
+alert("t1");
 /*var word=mes.split(" ");
 for(var i=0;i<word.length;i++) {
     t1.innerHTML=word[i];
